67440c787edda34645e3f355b9f3ba56660da033
berinaspirjan <berina.spirjan@treca-gimnazija.edu.ba>
2024-01-12 01:57:45 +0100

    Formatted a file

--- iterator.cpp
+++ iterator.cpp
@@ -5,25 +5,25 @@
 #include "stablo.h"
 #include <iostream>
 
-template <typename Tip>
+template<typename Tip>
 typename Stablo<Tip>::Iterator Stablo<Tip>::Iterator::najmanjiNaGrani() {
-    if(this->trenutni == nullptr)
+    if (this->trenutni == nullptr)
         return this; // ako je trenutni cvor nullptr, nema najmanjeg elementa, vracamo trenutni iterator (iterator na trenutni element ostaje nepromijenjen)
-    while(this->trenutni->ld != nullptr)
+    while (this->trenutni->ld != nullptr)
         this->trenutni = this->trenutni->ld; // prolazimo kroz lijevo podstablo dok ne dodjemo do najmanjeg elementa
     return this; // vracamo promijenjeni iterator na najmanji element
 }
 
-template <typename Tip>
+template<typename Tip>
 typename Stablo<Tip>::Iterator Stablo<Tip>::Iterator::najveciNaGrani() {
-    if(this->trenutni == nullptr)
+    if (this->trenutni == nullptr)
         return this; // ako je trenutni cvor nullptr, nema najmanjeg elementa, vracamo trenutni iterator (iterator na trenutni element ostaje nepromijenjen)
-    while(this->trenutni->dd != nullptr)
+    while (this->trenutni->dd != nullptr)
         this->trenutni = this->trenutni->dd;  // prolazimo kroz desno podstablo dok ne dodjemo do najveceg elementa
     return this; // vracamo promijenjeni iterator koji ce pokazivati na najveci element
 }
 
-template <typename Tip>
+template<typename Tip>
 typename Stablo<Tip>::Iterator Stablo<Tip>::Iterator::operator++() {
     if (this->trenutni->dd != nullptr) {
         // Ako postoji desno dijete, idemo na najmanji element u desnom podstablu
